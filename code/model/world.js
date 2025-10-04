aebca42a1b701e04a60faeaa00a5420df23def07
Alex Henry <tukkek@gmail.com>
2025-10-04 00:00:00 -0300

    Generate more-regionss then collapse them for a more natural-look. Also make only-10 final-regions. This wouldn't be generally-ideal but here we want to limit the number of enemies in an AOM match.

--- code/model/world.js
+++ code/model/world.js
@@ -19,6 +19,8 @@ class Cell{
   get x(){return this.point.x}
 
   get y(){return this.point.y}
+
+  distance(cell){return this.point.distance(cell.point)}
 }
 
 class Region{
@@ -26,6 +28,7 @@ class Region{
     this.cells=[]
     this.biome=false
     this.border=[]
+    this.center=false
   }
 
   expand(){
@@ -51,10 +54,30 @@ class Region{
       if(near.length>2) border.remove(cell)
     }
   }
+
+  seed(cell){
+    this.center=cell
+    this.cells.push(cell)
+  }
+
+  distance(region){return this.center.distance(region.center)}
+
+  recenter(){
+    let x=[]
+    let y=[]
+    let cells=this.cells
+    for(let cell of cells){
+      x.push(cell.x)
+      y.push(cell.y)
+    }
+    for(let axis of [x,y]) axis.sort((number1,number2)=>number1-number2)
+    let center=new pointm.Point(x[Math.floor(x.length/2)],y[Math.floor(y.length/2)])
+    this.center=cells.reduce((cell1,cell2)=>cell1.point.distance(center)<cell2.point.distance(center)?cell1:cell2)
+  }
 }
 
 export class World{
-  constructor(width=5**3,heigth=5**3,nregions=5**2){
+  constructor(width=5**3,heigth=5**3,nregions=100){
     this.width=width
     this.heigth=heigth
     this.regions=Array.create(nregions,()=>new Region())
@@ -67,13 +90,12 @@ export class World{
   shape(){
     let cells=rpgm.shuffle(this.cells.flat())
     let regions=this.regions
-    for(let region of regions) region.cells.push(cells.pop())
+    for(let region of regions) region.seed(cells.pop())
     cells=new Set(cells)
     let near=new Map()
     for(let region of regions) near.set(region,new Set(region.cells[0].expand()))
     while(cells.size){
-      regions.sort((region1,region2)=>region1.cells.length-region2.cells.length)
-      let region=rpgm.choose(regions,3)
+      let region=rpgm.pick(regions)
       let pool=near.get(region)
       if(!pool.size) continue
       let cell=rpgm.pick(Array.from(pool))
@@ -91,15 +113,28 @@ export class World{
     for(let near of region.expand()) this.form(near,biome)
   }
 
+  collapse(nregions=10){
+    let regions=this.regions
+    for(let region of regions) region.recenter()
+    while(regions.length>nregions){
+      let region=regions.reduce((region1,region2)=>region1.cells.length<region2.cells.length?region1:region2)
+      let annex=region.expand().reduce((region1,region2)=>region1.distance(region)<region2.distance(region)?region1:region2)
+      regions.remove(annex)
+      region.cells.push(...annex.cells)
+    }
+  }
+
   create(){
     performance.mark('world1')
     this.shape()
     this.form(rpgm.pick(this.regions))
+    this.collapse()
     for(let region of this.regions){
       region.close()
       for(let cell of region.cells) cell.region=region
     }
     performance.mark('world2')
     console.log('World creation',performance.measure('world','world1','world2').duration/1_000)
+    console.log('Most near-bys',Math.max(...this.regions.map((region)=>region.expand().length)))
   }
 }
